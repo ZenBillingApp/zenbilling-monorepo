a791b2ea76063409fff34780dc0ed9f3b7c6a364
Hassan <hassan@onlyonecard.eu>
2025-09-17 01:48:34 +0200

    chore: mise à jour de la configuration CORS dans le service d'authentification
    - Réintroduction de la configuration CORS dans le service d'authentification avec des origines spécifiques pour les environnements de développement.
    - Centralisation de la gestion CORS pour simplifier les requêtes cross-origin et améliorer la sécurité des endpoints API.

--- packages/auth_service/src/app.ts
+++ packages/auth_service/src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
-
+import cors from "cors";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.routes";
 
@@ -10,14 +10,18 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3001;
 
-// // Configure CORS middleware
-// app.use(
-//     cors({
-//         origin: process.env.CLIENT_URL!,
-//         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-//         credentials: true,
-//     })
-// );
+// Configure CORS middleware
+app.use(
+    cors({
+        origin: [
+            "http://localhost:3000",
+            "https://zenbilling-dev.dynamicwebforge.fr",
+            "https://zenbillingapi-dev.dynamicwebforge.fr",
+        ],
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+        credentials: true,
+    })
+);
 
 // Routes d'authentification Better Auth
 app.all("/api/auth/*splat", toNodeHandler(auth));
